21e94627f662d5197ab63de26a841fa0a4df06e0
Nurmukhanbet <nurmuhanbet2004@icloud.com>
2024-11-20 21:16:28 +0600

    Add Person component displaying person details

---
+++ react-task/src/Person.jsx
@@ -0,0 +1,23 @@
+const Person = () => {
+    const person = {
+        name: "Nurmukhanbet Alaibek uulu",
+        position: "Developer",
+        company: "O!",
+        experience: "2 years",
+        location: "Bishkek",
+        hobby: "Play Basketball"
+    };
+
+    return (
+        <div>
+            <h1>{person.name}</h1>
+            <p>Position: {person.position}</p>
+            <p>Company: {person.company}</p>
+            <p>Experience: {person.experience}</p>
+            <p>Location: {person.location}</p>
+            <p>Hobby: {person.hobby}</p>
+        </div>
+    );
+};
+
+export default Person;
