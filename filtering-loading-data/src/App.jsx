922322745b24ce5bf0690c8d1745ad8031fef31a
Nurmukhanbet <nurmuhanbet2004@icloud.com>
2024-12-01 18:21:21 +0600

    Filter and load data is added into main

---
+++ filtering-loading-data/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './App.css'
+import FilterableList from "./FilterableList";
+
+const App = () => {
+  return (
+    <div>
+      <FilterableList />
+    </div>
+  );
+};
+
+export default App;
---
+++ filtering-loading-data/src/FilterableList.jsx
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+
+const FilterableList = () => {
+  const [data, setData] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const mockData = [
+        "Apple",
+        "Banana",
+        "Cherry",
+        "Orange",
+        "Mango",
+        "Peach",
+        "Plum",
+        "Kiwi",
+        "Watermelon",
+        "Melon",
+      ];
+      setData(mockData);
+    };
+  
+    fetchData();
+  }, []);
+
+  const filteredData = data.filter((item) =>
+    item.toLowerCase().includes(searchTerm.toLowerCase())
+  );  
+
+  return (
+    <div>
+      <h1>Data filtering</h1>
+      <input
+      type="text"
+      placeholder="Search..."
+      value={searchTerm}
+      onChange={(e) => setSearchTerm(e.target.value)}
+      />
+
+      {searchTerm ? (
+        filteredData.length > 0 ? (
+          <div>
+            {filteredData.map((item, index) => (
+              <div key={index}>{item}</div>
+            ))}
+          </div>
+        ) : (
+          <p>Nothing found</p>
+        )
+      ) : (
+        <p></p>
+      )}
+    </div>
+  );
+};
+
+export default FilterableList;
