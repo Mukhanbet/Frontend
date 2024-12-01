2cb50dc9b748f2e8007258ae26b1d84781a7e2e2
Nurmukhanbet <nurmuhanbet2004@icloud.com>
2024-12-01 18:01:04 +0600

    Initialize data on first render is done

--- filtering-loading-data/src/FilterableList.jsx
+++ filtering-loading-data/src/FilterableList.jsx
@@ -4,6 +4,27 @@ const FilterableList = () => {
   const [data, setData] = useState([]);
   const [searchTerm, setSearchTerm] = useState("");
 
+  useEffect(() => {
+    const fetchData = async () => {
+      const mockData = [
+        "Apple",
+        "Banana",
+        "Cherry",
+        "Orange",
+        "Mango",
+        "Peach",
+        "Plum",
+        "Kiwi",
+        "Watermelon",
+        "Melon",
+      ];
+      setData(mockData);
+    };
+  
+    fetchData();
+  }, []);
+  
+
   return (
     <div>
       <h1>Data filtering</h1>
